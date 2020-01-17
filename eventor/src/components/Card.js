b11c9279e2234ac27d7c4d3f11ecc81d82ab9954
Jackie <jzwang2410@gmail.com>
2020-01-17 20:39:13 +1100

    extended card as separate component

---
+++ eventor/src/components/Card.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Dropdown, DropdownButton} from 'react-bootstrap';
+
+const CardContainer = styled.div`
+    width: 100%; 
+    height: 175px;   
+`;
+
+const Heading = styled.h4`
+    height: 30px;
+    padding: 10px;
+    padding-top: 3px;
+`;
+
+const Body = styled.div`
+    height: 132px;
+    margin: 5px;
+    text-align: left;
+    padding-left: 10px;
+    padding-top: 5px;
+    background-color: lightgray;
+    position: relative;
+`;
+
+const Tag = styled.div`
+    background-color: rgb(0,80,115); 
+    font-size: small;
+    color: white;
+    padding: 5px;
+    border-radius: 25px;
+    display: inline-block;
+    position: absolute;
+    bottom: 5%;
+`;
+
+
+const Card = (props) =>{
+  return (
+      <CardContainer>
+          <Heading className = "text-left">
+              {props.heading}
+              <DropdownButton
+              drop = "right" 
+              className = "float-right" 
+              size = "sm"
+              variant = "dark"
+              title = ""
+              >
+                  <Dropdown.Item 
+                  isSelected
+                  as="button" 
+                  onClick = {()=>{console.log("hello world")}}    
+                  >
+                      Edit
+                  </Dropdown.Item>
+                  <Dropdown.Item as="button" onClick = {props.delete}>Delete</Dropdown.Item>
+              </DropdownButton>
+          </Heading>
+          <Body>
+              <div>{props.body}</div>
+              <Tag>{props.event}</Tag>
+          </Body>
+      </CardContainer>
+  );
+}
+
+export default Card; 
--- eventor/src/components/Task.js
+++ eventor/src/components/Task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Draggable} from 'react-beautiful-dnd';
-import {Dropdown, DropdownButton} from 'react-bootstrap';
+import Card from './Card';
 
 const Container = styled.div`
     width: 280px;
@@ -9,40 +9,6 @@ const Container = styled.div`
     margin: 10px;
     background-color: ${props=> (props.isDragging ? 'skyblue': 'white')};
 `;
-
-const CardContainer = styled.div`
-    width: 100%; 
-    height: 175px;   
-`;
-
-const Heading = styled.h4`
-    height: 30px;
-    padding: 10px;
-    padding-top: 3px;
-`;
-
-const Body = styled.div`
-    height: 132px;
-    margin: 5px;
-    text-align: left;
-    padding-left: 10px;
-    padding-top: 5px;
-    background-color: lightgray;
-    position: relative;
-`;
-
-const Tag = styled.div`
-    background-color: rgb(0,80,115); 
-    font-size: small;
-    color: white;
-    padding: 5px;
-    border-radius: 25px;
-    display: inline-block;
-    position: absolute;
-    bottom: 5%;
-`;
-
-
 
 export default class Task extends React.Component{
 
@@ -93,31 +59,3 @@ export default class Task extends React.Component{
     }
 }
 
-const Card = (props) =>{
-    return (
-        <CardContainer>
-            <Heading className = "text-left">
-                {props.heading}
-                <DropdownButton
-                drop = "right" 
-                className = "float-right" 
-                size = "sm"
-                variant = "dark"
-                title = ""
-                >
-                    <Dropdown.Item 
-                    as="button" 
-                    onClick = {()=>{console.log("hello world")}}    
-                    >
-                        Edit
-                    </Dropdown.Item>
-                    <Dropdown.Item as="button" onClick = {props.delete}>Delete</Dropdown.Item>
-                </DropdownButton>
-            </Heading>
-            <Body>
-                <div>{props.body}</div>
-                <Tag>{props.event}</Tag>
-            </Body>
-        </CardContainer>
-    );
-}
