d83d455762eed9e35a46b0c5af9a788e2569e07b
Jackie <jzwang2410@gmail.com>
2020-01-18 16:46:35 +1100

    removed intialData file for taskboard

--- eventor/src/components/Taskboard.js
+++ eventor/src/components/Taskboard.js
@@ -1,111 +1,141 @@
-import React from 'react';
-import initialData from './initialData';
-import Column from './Column';
-import { DragDropContext } from 'react-beautiful-dnd';
-import styled from 'styled-components';
+import React from "react";
+import initialData from "./initialData";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const URL = "http://127.0.0.1:5468";
 
 const Container = styled.div`
-    display: flex; 
+  display: flex;
 `;
 
 class Taskboard extends React.Component {
-    state = initialData;
+  state = {
+    loading: true,
+    taskboard: null
+  };
 
-    //Changes text when drag starts
-    onDragStart = () => {
-        // document.body.style.color = 'orange';
-        // document.body.style.transition = 'background-color 0.2 ease';
-        // document.body.style.backgroundColor = 'rgb(199,180,142)';
-    }
+  async componentDidMount() {
+    const options = {
+      headers: { "Content-Type": "application/json" }
+    };
 
-    // Changing background opacity
-    onDragUpdate = update => {
-        // const {destination} = update;
-        // const opacity = 
-        //     destination ? destination.index / Object.keys(this.state.tasks).length : 0;
-        // document.body.style.backgroundColor = 'rgb(199,180,142)';
-    }
+    const response = await fetch(`${URL}/task`, options);
+    const payload = await response.json();
+    this.setState({
+        loading: false,
+      taskboard: payload
+    });    
+  }
 
-    onDragEnd = result => {
+  //Changes text when drag starts
+  onDragStart = () => {
+    // document.body.style.color = 'orange';
+    // document.body.style.transition = 'background-color 0.2 ease';
+    // document.body.style.backgroundColor = 'rgb(199,180,142)';
+  };
 
-        //Resets text and background color to initial color when drag ends
-        document.body.style.color = 'inherit';
-        document.body.style.backgroundColor = 'inherit';
+  // Changing background opacity
+  onDragUpdate = update => {
+    // const {destination} = update;
+    // const opacity =
+    //     destination ? destination.index / Object.keys(this.state.taskboard.tasks).length : 0;
+    // document.body.style.backgroundColor = 'rgb(199,180,142)';
+  };
 
-        const { destination, source, draggableId } = result;
-        if (!destination) return;
-        if (destination.droppableId === source.droppableId &&
-            destination.index === source.index) return;
+  onDragEnd = result => {
+    //Resets text and background color to initial color when drag ends
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
 
-        const start = this.state.columns[source.droppableId]
-        const finish = this.state.columns[destination.droppableId]
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
 
-        //If moving to same list
-        if (start === finish) {
-            const newTaskIds = Array.from(start.taskIds);
-            newTaskIds.splice(source.index, 1);
-            newTaskIds.splice(destination.index, 0, draggableId);
+    const start = this.state.taskboard.columns[source.droppableId];
+    const finish = this.state.taskboard.columns[destination.droppableId];
 
-            const newColumn = {
-                ...start,
-                taskIds: newTaskIds
-            };
+    //If moving to same list
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
 
-            const newState = {
-                ...this.state,
-                columns: {
-                    ...this.state.columns,
-                    [newColumn.id]: newColumn,
-                },
-            };
-            this.setState(newState);
-            return;
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      };
+
+      const newState = {
+        ...this.state.taskboard,
+        columns: {
+          ...this.state.taskboard.columns,
+          [newColumn.id]: newColumn
         }
-
-        //Moving from one list to another
-        const startTaskIds = Array.from(start.taskIds);
-        startTaskIds.splice(source.index, 1);
-        const newStart = {
-            ...start,
-            taskIds: startTaskIds,
-        };
-
-        const finishTaskIds = Array.from(finish.taskIds);
-        finishTaskIds.splice(destination.index, 0, draggableId);
-        const newFinish = {
-            ...finish,
-            taskIds: finishTaskIds,
-        };
-
-        const newState = {
-            ...this.state,
-            columns: {
-                ...this.state.columns,
-                [newStart.id]: newStart,
-                [newFinish.id]: newFinish,
-            },
-        };
-
-        this.setState(newState);
+      };
+      this.setState(newState);
+      return;
     }
 
-    render() {
-        return (
-            <DragDropContext
-                onDragEnd={this.onDragEnd}
-                onDragStart={this.onDragStart}
-                onDragUpdate={this.onDragUpdate}
-            >
-                <Container>
-                    {this.state.columnOrder.map(columnId => {
-                        const column = this.state.columns[columnId];
-                        const task = column.taskIds.map((taskId) => this.state.tasks[taskId]);
-                        return <Column key={column.id} column={column} tasks={task}></Column>
-                    })}
-                </Container>
-            </DragDropContext>
-        )
-    }
+    //Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
+    };
+
+    const newState = {
+      ...this.state.taskboard,
+      columns: {
+        ...this.state.taskboard.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
+      }
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <DragDropContext
+            onDragEnd={this.onDragEnd}
+            onDragStart={this.onDragStart}
+            onDragUpdate={this.onDragUpdate}
+          >
+            <Container>
+              {this.state.taskboard.columnOrder.map(columnId => {
+                const column = this.state.taskboard.columns[columnId];
+                const task = column.taskIds.map(
+                  taskId => this.state.taskboard.tasks[taskId]
+                );
+                return (
+                  <Column key={column.id} column={column} tasks={task}></Column>
+                );
+              })}
+            </Container>
+          </DragDropContext>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Taskboard;
--- restApi/index.js
+++ restApi/index.js
@@ -5,31 +5,31 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const data = {
-    tasks : {
-        'task-1' : { id: 'task-1', content: 'fix bathroom'},
-        'task-2' : { id: 'task-2', content: 'watch show'},
-        'task-3' : { id: 'task-3', content: 'eat dinner'},
-        'task-4' : { id: 'task-4', content: 'punch wall'}
-    },
-    columns : {
-        'column-1' :{
-            id: 'column-1',
-            title: 'To-do',
-            taskIds : ['task-1','task-2','task-3','task-4']
-        },
-        'column-2' :{
-            id: 'column-2',
-            title: 'In progress', 
-            taskIds: [],
-        },
-        'column-3' :{
-            id: 'column-3',
-            title: 'Done',
-            taskIds: [],
-        }
-    },
-    columnOrder : ['column-1','column-2','column-3'], 
+const initialData = {
+	tasks: {
+		'task-1': { id: 'task-1', content: 'fix bathroom', event: 'General Fix' },
+		'task-2': { id: 'task-2', content: 'watch show', event: 'Entertainment' },
+		'task-3': { id: 'task-3', content: 'eat dinner', event: 'Entertainment' },
+		'task-4': { id: 'task-4', content: 'punch wall', event: 'Accident' }
+	},
+	columns: {
+		'column-1': {
+			id: 'column-1',
+			title: 'To-do',
+			taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+		},
+		'column-2': {
+			id: 'column-2',
+			title: 'In progress',
+			taskIds: [],
+		},
+		'column-3': {
+			id: 'column-3',
+			title: 'Done',
+			taskIds: [],
+		}
+	},
+	columnOrder: ['column-1', 'column-2', 'column-3'],
 }
 
 app.get('/', (req,res) =>{
@@ -46,6 +46,9 @@ app.get('/task/:id',(req,res)=>{
     res.send(task);    
 });
 
+app.get('/task', (req,res)=>{
+    res.send(initialData); 
+})
 app.post('/task', (req,res)=>{
 
     console.log(req.body);
